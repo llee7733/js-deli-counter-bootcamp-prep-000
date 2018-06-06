877185779f564632e21887b2416278365b096adb
Lloyd Lee <kinglloydlee@gmail.com>
2018-06-06 05:03:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -29,34 +29,7 @@ for(let i = 1; i<katzDeliLine.length;i++){
 }
 return myString
 }
-=======
-function takeANumber(katzDeliLine, name) {
-  katzDeli.push('${name}');
-  return ('Welcome,. ${name}. You are number ${katzDeliLine.length} in line.');
-}
 
-function nowServing(katzDeli) {
-  let i = 0;
-    while (i < katzDeli.length) {
-      i++;
-}
-if (katzDeli.length === 0) {
-  return "There is nobody waiting to be served!";
-}
-else
-  return (`Currently serving ${katzDeli.shift()}.`);
-}
-
-var line = [];
-function currentLine(katzDeli) {
-  let i = 0;
-    while (i < katzDeli.length) {
-      line.push(` `+[i+1]+`. `  + katzDeli[i])
-    i++;
-}
-if (katzDeli.length === 0) {
-  return "The line is currently empty.";
-} 
 else
   return(`The line is currently:` + line);
 }
