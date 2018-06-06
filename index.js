5137810c66f6eb4e7d3b44323603a8f498bf7bdd
Lloyd Lee <kinglloydlee@gmail.com>
2018-06-06 05:01:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var katzDeliLine = [];
+<<<<<<< HEAD
 
 function takeANumber(katzDeliLine, name) {
   var position = katzDeliLine.length+1
@@ -28,3 +29,35 @@ for(let i = 1; i<katzDeliLine.length;i++){
 }
 return myString
 }
+=======
+function takeANumber(katzDeliLine, name) {
+  katzDeli.push('${name}');
+  return ('Welcome,. ${name}. You are number ${katzDeliLine.length} in line.');
+}
+
+function nowServing(katzDeli) {
+  let i = 0;
+    while (i < katzDeli.length) {
+      i++;
+}
+if (katzDeli.length === 0) {
+  return "There is nobody waiting to be served!";
+}
+else
+  return (`Currently serving ${katzDeli.shift()}.`);
+}
+
+var line = [];
+function currentLine(katzDeli) {
+  let i = 0;
+    while (i < katzDeli.length) {
+      line.push(` `+[i+1]+`. `  + katzDeli[i])
+    i++;
+}
+if (katzDeli.length === 0) {
+  return "The line is currently empty.";
+} 
+else
+  return(`The line is currently:` + line);
+}
+>>>>>>> 88baa164efb56f0f19ec3b806b341f88c94459ca
